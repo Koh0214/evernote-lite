687c0aca1671c44d326a25ef6533d29252964c8f
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-11 16:07:47 +0900

    rails g model note & be rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211065757) do
+ActiveRecord::Schema.define(version: 20170211070126) do
 
   create_table "note_folders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "title"
+    t.text     "body",           limit: 65535
+    t.integer  "note_folder_id"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
