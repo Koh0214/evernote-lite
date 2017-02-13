ddefe30a1df456b3c84dd50bcaff8110eaa98383
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-13 18:38:01 +0900

    note_folders_controller | create new, create, set_params method

--- app/controllers/note_folders_controller.rb
+++ app/controllers/note_folders_controller.rb
@@ -1,4 +1,21 @@
 class NoteFoldersController < ApplicationController
+
   def index
+  end
+
+  def new
+    @note_folder = NoteFolder.new
+  end
+
+  def create
+    @note_folder = NoteFolder.create(set_params)
+    redirect_to :root
+  end
+
+
+
+  private
+  def set_params
+    params.require(:note_folder).permit(:name).merge(user_id: current_user.id)
   end
 end
