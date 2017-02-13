2389d7d4af7cff82509b6ee2a971703198cbc34c
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-13 18:42:20 +0900

    set validation name to user.rb and note_folder.rb

--- app/models/note_folder.rb
+++ app/models/note_folder.rb
@@ -1,4 +1,6 @@
 class NoteFolder < ApplicationRecord
   belongs_to  :user
   has_many    :notes
+
+  validates :name, presence: true #nameはdeviseでvalidationされない。
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :note_folders
+
+  validates :name, presence: true #nameはdeviseでvalidationされない。
 end
