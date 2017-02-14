be7390b2cf6d506a67a854efeff7ab0d1d4381fd
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-14 19:35:03 +0900

    notes_controller.rb | set @note into index action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,11 @@
 class NotesController < ApplicationController
   def index
+    @note = current_user.notes.last
     @notes = current_user.notes
   end
 
   def show
-    @note = Note.find(params[:id]) #ここのidはnote_folderのid
+    @note = Note.find(params[:id]) #ここのidはnoteのid
     @note_folder = NoteFolder.find(@note.note_folder_id)
     @notes = Note.where(note_folder_id: @note.note_folder_id).order("updated_at DESC")
   end
