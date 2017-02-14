8536f85c9c1a3ca33d325354fd90f6a184425bc5
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-14 14:56:22 +0900

    notes_controller.rb | add show method and set @note, @notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,9 @@
 class NotesController < ApplicationController
   def index
   end
+
+  def show
+    @note = Note.find(params[:id])
+    @notes = Note.where(note_folder_id: @note.note_folder_id)
+  end
 end
