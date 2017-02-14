d29e7827dbbf9acec07e3fe9528f65495a76effe
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-14 17:09:45 +0900

    notes_controller.rb | set @note_folder and @notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,7 @@ class NotesController < ApplicationController
 
   def show
     @note = Note.find(params[:id])
-    @notes = Note.where(note_folder_id: @note.note_folder_id)
+    @note_folder = NoteFolder.find(@note.note_folder_id)
+    @notes = Note.where(note_folder_id: @note.note_folder_id).order("updated_at DESC")
   end
 end
