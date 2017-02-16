828da3caed8015539344ee0641fa7e61e2c563e1
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-16 17:56:36 +0900

    notes_controller.rb | set before_action set_note_folder

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+  before_action :set_note_folder, except: [:index]
+
   def index
     @note = current_user.notes.last
     @notes = current_user.notes.order("updated_at DESC")
@@ -6,11 +8,9 @@ class NotesController < ApplicationController
 
   def new
     @note = Note.new
-    @note_folder = NoteFolder.find(params[:note_folder_id])
   end
 
   def create
-    @note_folder = NoteFolder.find(params[:note_folder_id])
     @note = Note.new(set_params)
     if @note.save
       redirect_to note_folder_path(@note_folder)
@@ -21,23 +21,20 @@ class NotesController < ApplicationController
 
   def show
     @note = Note.find(params[:id]) #ここのidはnoteのid
-    @note_folder = NoteFolder.find(@note.note_folder_id)
-    @notes = Note.where(note_folder_id: @note.note_folder_id).order("updated_at DESC")
+    @notes = Note.where(note_folder_id: params[:note_folder_id]).order("updated_at DESC")
   end
 
   def destroy
     @note = Note.find(params[:id])
     @note.destroy
-    redirect_to :root
+    redirect_to note_folder_path(@note_folder)
   end
 
   def edit
     @note = Note.find(params[:id])
-    @note_folder = NoteFolder.find(params[:note_folder_id])
   end
 
   def update
-    @note_folder = NoteFolder.find(params[:note_folder_id])
     if @note = Note.update(set_params)
       redirect_to note_folder_path(@note_folder)
     else
@@ -50,5 +47,9 @@ class NotesController < ApplicationController
   private
   def set_params
     params.require(:note).permit(:title, :body).merge(user_id: current_user.id, note_folder_id: params[:note_folder_id])
+  end
+
+  def set_note_folder
+    @note_folder = NoteFolder.find(params[:note_folder_id])
   end
 end
