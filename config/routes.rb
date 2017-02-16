c56afcf51b205b2a032f5277ed9b9e3ad4750df1
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-16 15:54:12 +0900

    routes.rb | nest notes in note_folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'notes#index'
-  resources :note_folders
-  resources :notes
+  resources :note_folders do
+    resources :notes
+  end
 end
