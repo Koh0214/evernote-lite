5b99b36f711b20567d665aa44cd901b40cd4ed9e
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-17 16:53:04 +0900

    remove setInterval autosave,  add selecter to mouse out auto-save

--- app/assets/javascripts/note_auto_save.js
+++ app/assets/javascripts/note_auto_save.js
@@ -1,27 +1,6 @@
 $(function(){
-
-  setInterval(function() {
-    var note_id = gon.note_id
-    var note_folder_id = gon.note_folder_id
-    var formData = new FormData($('.edit_note').get(0))
-
-    $.ajax({
-      type: 'PUT',
-      url: '/note_folders/' + note_folder_id + '/notes/' + note_id,
-      data: formData,
-      processData: false,
-      contentType: false,
-      dataType: 'json'
-    })
-    .done(function(data) {
-      console.log('saved');
-    })
-    .fail(function(data) {
-      console.log('送信失敗');
-    });
-  },3000);
-
-  $('.form-content__body__text').mouseout(function() {
+  
+  $('.form-content__header__note-title, .form-content__body__text').mouseout(function() {
     var note_id = gon.note_id
     var note_folder_id = gon.note_folder_id
     var formData = new FormData($('.edit_note').get(0))
@@ -34,7 +13,7 @@ $(function(){
       dataType: 'json'
     })
     .done(function(data) {
-      console.log('mouseout save');
+      console.log('mouseout save(title)');
     })
     .fail(function(data) {
       console.log('送信失敗');
