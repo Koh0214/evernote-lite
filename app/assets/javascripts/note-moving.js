2c6357247874b984c201539f3179e356c893a9e4
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-19 01:39:50 +0900

    note-moving.js | remove move animate(only BGC and shadow change)

--- app/assets/javascripts/note-moving.js
+++ app/assets/javascripts/note-moving.js
@@ -1,16 +1,12 @@
 $(function(){
 
-  $('.notes__body__box').on('click', function(){
+  $('.notes__body').on('click','.notes__body__box', function(){
     $(this).animate({ backgroundColor: '#ffffff' }, 100, "easeInCubic")
     $(this).animate({ boxShadow: '0 0 30px #5ce050' }, 300, "easeInCubic")
-    $(this).animate({ marginLeft: 10 }, 300, "easeInCubic")
-    $(this).animate({ width: 285 }, 100, "easeInCubic")
 
-    $(this).parent().siblings().children().animate({ backgroundColor: '#e0e3e0' }, 300, "easeInCubic")
-    $(this).parent().siblings().children().animate({ marginLeft: 0 }, 400, "easeInCubic")
+    // このコードは重すぎるから、クラスとかでいい感じに判定を入れる
+    $(this).parent().siblings().children().animate({ backgroundColor: '#e6ece6' }, 300, "easeInCubic")
     $(this).parent().siblings().children().animate({ boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.3)' }, 400, "easeInCubic")
-    $(this).parent().siblings().children().animate({ width: 290 }, 1000, "easeInCubic")
-
   })
 
 });
