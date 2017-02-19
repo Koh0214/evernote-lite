8126314386101db33484e00f6257a70327681d4a
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-19 16:33:39 +0900

    note_auto_save.js | add new note auto-save

--- app/assets/javascripts/note_auto_save.js
+++ app/assets/javascripts/note_auto_save.js
@@ -19,7 +19,13 @@ $(function(){
   // },1000);
 
   $('.wrapper').on("mouseout", ".form-content__header__note-title", function() {
-    var formData = new FormData($('.edit_note').get(0))
+    // new_noteを作った後に、formのclassが変わるのでその対応
+    if ($('.new_note').length === 0) {
+      var formData = new FormData($('.edit_note').get(0))
+    }
+    else{
+      var formData = new FormData($('.new_note').get(0))
+    }
     $.ajax({
       type: 'PUT',
       url: window.location.pathname, //'/note_folders/' + note_folder_id + '/notes/' + note_id,
@@ -37,10 +43,16 @@ $(function(){
   });
 
   $('.wrapper').on("mouseout", ".form-content__body__text", function() {
-    var formData = new FormData($('.edit_note').get(0))
+    // new_noteを作った後に、formのclassが変わるのでその対応
+    if ($('.new_note').length === 0) {
+      var formData = new FormData($('.edit_note').get(0))
+    }
+    else{
+      var formData = new FormData($('.new_note').get(0))
+    }
     $.ajax({
       type: 'PUT',
-      url: window.location.pathname,
+      url: window.location.pathname, //'/note_folders/' + note_folder_id + '/notes/' + note_id,
       data: formData,
       processData: false,
       contentType: false,
@@ -54,7 +66,6 @@ $(function(){
     });
   });
 
-  // $('.wrapper').on("mouseout", ".form-content__header__note-title", function()
   // ビューのリロード時に出来てしまう空白を削除
   $('.form-content__body__text').ready(function(){
     $('.form-content__body__text').each(function(){
