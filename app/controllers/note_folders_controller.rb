7a2066f9a0e375bc3f7a8e8ae23744c9fce777e5
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-20 16:17:34 +0900

    notes&note_folders_controller.rb | add authenticate_user!

--- app/controllers/note_folders_controller.rb
+++ app/controllers/note_folders_controller.rb
@@ -1,4 +1,5 @@
 class NoteFoldersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_note_folder, only: [:show, :edit]
 
   def index
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_note_folder, except: [:index]
 
   def index
