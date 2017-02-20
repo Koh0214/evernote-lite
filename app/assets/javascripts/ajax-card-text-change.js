c7c0c738ad2701d8c1f52a074c143447f1f929eb
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-20 16:00:47 +0900

    ajax-card-text-change.js | add if state title&text is blank

--- app/assets/javascripts/ajax-card-text-change.js
+++ app/assets/javascripts/ajax-card-text-change.js
@@ -10,8 +10,13 @@ $(function() {
         old = v;
         isChange = true;
         var changed_title = $('.form-content__header__note-title').val();
-        $('.editing').find('.card__title').text(changed_title);
-        $('.editing .notes__body__box__no-title').attr('class','notes__body__box__title card__title');
+        if (changed_title == 0) {
+          $('.editing').find('.card__title').text('タイトルがありません');
+          $('.editing .notes__body__box__title').attr('class','notes__body__box__no-title card__title');
+        } else {
+          $('.editing').find('.card__title').text(changed_title);
+          $('.editing .notes__body__box__no-title').attr('class','notes__body__box__title card__title');
+        }
       }
     }
   }
@@ -24,8 +29,13 @@ $(function() {
         old = v;
         isChange = true;
         var changed_text = $('.form-content__body__text').val();
-        $('.editing').find('.card__body').text(changed_text);
-        $('.editing .notes__body__box__no-body').attr('class','notes__body__box__body card__body');
+        if (changed_text == 0) {
+          $('.editing').find('.card__body').text('本文がありません');
+          $('.editing .notes__body__box__body').attr('class','notes__body__box__no-body card__body');
+        } else {
+          $('.editing').find('.card__body').text(changed_text);
+          $('.editing .notes__body__box__no-body').attr('class','notes__body__box__body card__body');
+        }
       }
     }
   }
