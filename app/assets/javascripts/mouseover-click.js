2643268f9d385b6bb61556d8636a703d9a93e8f9
岸本 晃平2 <Kohei2@koheimba.local>
2017-03-02 19:07:10 +0900

    mouseover-click.js | set mouover focus on note-title&body

--- app/assets/javascripts/mouseover-click.js
+++ app/assets/javascripts/mouseover-click.js
@@ -4,4 +4,17 @@ $(function(){
   $('.notes__body__box').on('mouseover', function(){
     $(this)[0].click();
   })
+
+  $('.wrapper').on('mouseover','.form-content__header__note-title', function(){
+    var txt = $(this).val();
+
+    // カーソルを一番最後に持ってくる
+    $(this).val('');
+    $(this)[0].focus()
+    $(this).val(txt);
+  })
+
+  $('.wrapper').on('mouseover','.form-content__body__text', function(){
+    $(this)[0].focus();
+  })
 })
