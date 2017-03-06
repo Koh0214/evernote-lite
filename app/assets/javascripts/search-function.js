9e58b15e2e2c92550d608b284531b34eefd3b7e0
岸本 晃平2 <Kohei2@koheimba.local>
2017-03-06 17:19:45 +0900

    search-function.js | add build_card function

--- app/assets/javascripts/search-function.js
+++ app/assets/javascripts/search-function.js
@@ -1,5 +1,30 @@
 $(function(){
 
+  function buildCard(id, title, body) {
+    console.log('buildCard');
+    $(
+      '<a href="/note_folders/' + gon.note_folder_id + '/notes/' + id + '">' +
+        '<div class="notes__body__box">' +
+          '<div class="notes__body__box__title card__title"></div>' +
+          '<hr class="notes__body__box__line">' +
+          '<div class="notes__body__box__body card__body"></div>' +
+        '</div>' +
+      '</a>'
+    )
+    .find('.card__title').text(title).end()
+    .find('.card__body').text(body).end()
+    .prependTo($('.notes__body'))
+  };
+
+  function buildCardList(cards) {
+    console.log('buildList');
+    $.each(cards,
+      function (index, card) {
+        buildCard(card.id, card.title, card.body)
+      }
+    );
+  };
+
   $('.notes__search-bar').on("keyup", function() {
 
     var input_text = $('.notes__search-bar').val();
@@ -11,8 +36,10 @@ $(function(){
       data: { text: input_text },
       dataType: 'json'
     })
-    .done(function(data) {
-      console.log(data);
+    .done(function(notes) {
+      console.log(notes);
+      $('.notes__body__box').remove();
+      buildCardList(notes);
     })
     .fail(function(data) {
       console.log('送信失敗 text');
