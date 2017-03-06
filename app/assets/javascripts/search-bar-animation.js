5083507ef8ae8e5405703dc154612eddc8a447f4
岸本 晃平2 <Kohei2@koheimba.local>
2017-03-06 17:39:03 +0900

    search-bar-animation.js, scss | animation fine fix

--- app/assets/javascripts/search-bar-animation.js
+++ app/assets/javascripts/search-bar-animation.js
@@ -2,14 +2,14 @@ $(function(){
 
   $('.wrapper').on('click mouseover', '.not-searching', function(){
     $('.notes__search-bar').val('')
-    $('.notes__search-bar').animate({ left: 8, top: 10, width: 245, opacity: 1}, 300, "easeInCubic")
+    $('.notes__search-bar').animate({ left: 8, top: 10, width: 245, opacity: 1}, 500, "easeOutExpo")
     $(this).addClass('searching')
     $(this).removeClass('not-searching')
     $('.notes__search-bar')[0].focus();
   })
 
   $('.wrapper').on('click', '.searching', function(){
-    $('.notes__search-bar').animate({ left: 295, top: 14, width: 10, opacity: 0}, 300, "easeInCubic")
+    $('.notes__search-bar').animate({ left: 295, top: 10, width: 10, opacity: 0}, 300, "easeInCubic")
     $(this).removeClass('searching')
     $(this).addClass('not-searching')
   })
